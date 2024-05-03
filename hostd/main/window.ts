5fd7d0b043dc53b8f5feedcbdc83e02ad0bcc404
Alex Freska <alex.freska@gmail.com>
2024-05-03 14:36:59 -0400

    fix: renterer asset path

--- hostd/main/window.ts
+++ hostd/main/window.ts
@@ -30,7 +30,7 @@ export function initWindow() {
   const url = env.isDev
     ? 'http://localhost:8000/'
     : format({
-        pathname: path.join(__dirname, '../renderer/out/index.html'),
+        pathname: path.join(__dirname, '../../renderer/out/index.html'),
         protocol: 'file:',
         slashes: true,
       })

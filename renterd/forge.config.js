1911963e724985dc6695f1eedb6612146e472baf
Alex Freska <alex.freska@gmail.com>
2024-05-14 11:36:00 -0400

    fix: package daemon extra resources

--- renterd/forge.config.js
+++ renterd/forge.config.js
@@ -8,7 +8,7 @@ module.exports = {
       appleApiKeyId: process.env.APPLE_API_KEY,
       appleApiIssuer: process.env.APPLE_API_ISSUER,
     },
-    extraResource: ['daemon/**'],
+    extraResource: ['daemon/bin', 'daemon/version'],
     icon: './assets/icons/icon',
   },
   rebuildConfig: {},

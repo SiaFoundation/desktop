c429ab85af1f049bd3ccae537a13996b01ab01ca
Alex Freska <alex.freska@gmail.com>
2024-05-17 18:17:08 -0400

    ci: remove windows squirrel noDelta

--- renterd/forge.config.js
+++ renterd/forge.config.js
@@ -31,7 +31,7 @@ module.exports = {
       platforms: ['win32'],
       config: (arch) => ({
         // Only needs to be set to true for first publish ever.
-        noDelta: true,
+        // noDelta: true,
         remoteReleases: `https://releases.s3.sia.tools/renterd/win32/${arch}`,
         // An URL to an ICO file to use as the application icon (displayed in Control Panel > Programs and Features).
         iconUrl: 'https://sia.tech/assets/appicon.ico',
